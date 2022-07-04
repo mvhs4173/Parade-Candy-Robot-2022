06b84bbfd8b5f4a9f6a5cb99c74146dde298c4f1
Dallin-R16 <mvhs4173@gmail.com>
2022-07-03 20:30:54 -0700

    Increased launch LED trail length

--- Parade-Candy-Robot-2022/src/main/java/frc/robot/commands/LaunchCandy.java
+++ Parade-Candy-Robot-2022/src/main/java/frc/robot/commands/LaunchCandy.java
@@ -52,7 +52,7 @@ public class LaunchCandy extends CommandBase {
         cmdFlashLEDs.cancel();
       }
     } else {
-      RobotContainer.ledStrip.chaseTrail(10, 6, 126, 152);
+      RobotContainer.ledStrip.chaseTrail(20, 6, 126, 152);
     }
   }
 
